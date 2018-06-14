1fe1c93a0c2fb1424a78bb6cd0cf736cca51b658
Grady Phillips <infamy@unyx.io>
2018-06-14 19:27:13 -0400

    final commit

--- deploy.sh
+++ deploy.sh
@@ -13,19 +13,24 @@ cd nodeblox-api-server/
 echo 'Installing NodeJS deps.'
 npm install>/dev/null
 
-echo 'Installing systemctl service.'
-cp nodeblox-api-server.service /etc/systemd/system/nodeblox-api-server.service
-systemctl enable nodeblox-api-server>/dev/null
-systemctl start nodeblox-api-server>/dev/null
-
+echo 'Installing NVM.'
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh>/dev/null
 bash install_nvm.sh>/dev/null
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+echo 'Installing NodeJS v10.0.0.'
 nvm install 10.0.0 >/dev/null
 nvm use 10.0.0 >/dev/null
 node -v
 
+echo 'Installing systemctl service.'
+cp nodeblox-api-server.service /etc/systemd/system/nodeblox-api-server.service
+systemctl enable nodeblox-api-server>/dev/null
+
+echo 'Starting up Nodeblox Server.'
+systemctl start nodeblox-api-server>/dev/null
+
+echo 'Cleaning up directory.'
 rm -rf install_nvm.sh
