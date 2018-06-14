557783f5b274b2392e2127802ef0a9b9a9a334f2
Infamy <infamy@unyx.io>
2018-06-14 18:36:14 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,35 +2,35 @@
 # Deploy script by Grady Phillips
 
 echo 'Updating packages.'
-apt-get update >/dev/null
-apt-get upgrade -y >/dev/null
+apt-get -y update
+apt-get -y upgrade
 
 echo 'Installing package deps.'
-apt-get install nodejs npm git build-essential >/dev/null
-git clone https://github.com/winfamy/nodeblox-api-server.git>/dev/null
+apt-get -y -qq install nodejs npm git build-essential
+git clone https://github.com/winfamy/nodeblox-api-server.git
 cd nodeblox-api-server/
 
 echo 'Installing NodeJS deps.'
-npm install>/dev/null
+npm install
 
 echo 'Installing NVM.'
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh>/dev/null
-bash install_nvm.sh>/dev/null
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh -o install_nvm.sh
+bash install_nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 echo 'Installing NodeJS v10.0.0.'
-nvm install 10.0.0 >/dev/null
-nvm use 10.0.0 >/dev/null
+nvm install 10.0.0
+nvm use 10.0.0
 node -v
 
 echo 'Installing systemctl service.'
 cp nodeblox-api-server.service /etc/systemd/system/nodeblox-api-server.service
-systemctl enable nodeblox-api-server>/dev/null
+systemctl enable nodeblox-api-server
 
 echo 'Starting up Nodeblox Server.'
-systemctl start nodeblox-api-server>/dev/null
+systemctl start nodeblox-api-server
 
 echo 'Cleaning up directory.'
 rm -rf install_nvm.sh
